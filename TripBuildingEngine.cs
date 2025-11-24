b2327166a7bbc8f6d55b97bb53be0a7fce4a7af6
ChristopherDesrosiersMondor <desrosch@gmail.com>
2025-11-24 11:17:15 -0500

    Update TripBuildingEngine.cs

--- TripBuildingEngine.cs
+++ TripBuildingEngine.cs
@@ -1,18 +1,43 @@
-private async Task<TripEntity> CreateNewTrip(
-    Booking booking,
-    CreateTripCommand createTripCommand,
-    List<CreateBookingMetaCommand> additionalBookingMeta)
+public class TripsBuildingEngine : ITripsBuildingEngine
 {
-    await _mediatorSender.Send(new UpdateBookingCommand
+    private readonly ICertifyWebService _certifyService;
+    private readonly ICompanyApiEmployeeRepository _employeeRepository;
+    private readonly ISender _mediatorSender;
+    private readonly TimeSpan _openEndedTripEnd = TimeSpan.FromDays(7);
+    private readonly TimeSpan _openEndedTripStart = TimeSpan.FromDays(-7);
+    private readonly ICompanyApiTravelPolicyRepository _travelPolicyRepository;
+    private readonly ITripIssuesEnforcerService _tripIssuesEnforcerService;
+    private readonly ITripRepository _tripRepository;
+    private readonly ITfbApolloConsumerService _tfbApolloConsumerService;
+
+    public TripsBuildingEngine(ISender mediatorSender, ICompanyApiEmployeeRepository employeeRepository,
+        ICertifyWebService certifyService, ICompanyApiTravelPolicyRepository travelPolicyRepository,
+        ITripIssuesEnforcerService tripIssuesEnforcerService, ITripRepository tripRepository,
+        ITfbApolloConsumerService tfbApolloConsumerService)
     {
-        BookingId = booking.Id,
-        BookingMetas = additionalBookingMeta
-    });
-
-    // This will handle the trip creation
-    var createdTrip = await _mediatorSender.Send(createTripCommand);
-
-    return createdTrip;
+        _mediatorSender = mediatorSender;
+        _employeeRepository = employeeRepository;
+        _certifyService = certifyService;
+        _travelPolicyRepository = travelPolicyRepository;
+        _tripIssuesEnforcerService = tripIssuesEnforcerService;
+        _tripRepository = tripRepository;
+        _tfbApolloConsumerService = tfbApolloConsumerService;
+    }
+    
+    private async Task<TripEntity> CreateNewTrip(
+        Booking booking,
+        CreateTripCommand createTripCommand,
+        List<CreateBookingMetaCommand> additionalBookingMeta)
+    {
+        await _mediatorSender.Send(new UpdateBookingCommand
+        {
+            BookingId = booking.Id,
+            BookingMetas = additionalBookingMeta
+        });
+    
+        // This will handle the trip creation
+        var createdTrip = await _mediatorSender.Send(createTripCommand);
+    
+        return createdTrip;
+    }
 }
-
-// See UpdateBookingCommand file
